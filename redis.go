3386839a2555239e48c122c903b4f28c2a6d1bf5
Naru <omarnaru2002@gmail.com>
2025-11-16 13:35:59 +0200

    feat(redis): add Redis connection and cached-domain helpers

--- redis.go
+++ redis.go
@@ -2,6 +2,7 @@ package ainaa
 
 import (
 	"context"
+	"encoding/json"
 	"os"
 
 	"github.com/redis/go-redis/v9"
@@ -22,4 +23,24 @@ func connectRedis(ctx context.Context) (*redis.Client, error) {
 		return nil, err
 	}
 	return client, nil
+}
+
+func parseJsonCachedDomain(data string) (CachedDomain, error) {
+	var cachedDomains []CachedDomain
+	err := json.Unmarshal([]byte(data), &cachedDomains)
+	if err != nil {
+		return CachedDomain{}, err
+	}
+	return cachedDomains[0], nil
+}
+
+func getCachedDomain(ctx context.Context, client *redis.Client, domain string) (CachedDomain, error) {
+	if val, err := client.JSONGet(ctx, domain).Result(); err == nil {
+		cachedVal, err := parseJsonCachedDomain(val)
+		if err != nil {
+			return CachedDomain{}, err
+		}
+		return cachedVal, nil
+	}
+	return CachedDomain{}, redis.Nil
 }
